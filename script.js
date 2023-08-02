e7c5b2a16c6668dab0a91146ef43ca69be29c6c8
AARIYAN AJIM DEVELOPER <140634500+ARIYAN-AJIM@users.noreply.github.com>
2023-08-02 22:37:49 +0600

    Add files via upload
    Gsap animation on landing page love to make suzanna zumpel

---
+++ script.js
@@ -0,0 +1,26 @@
+gsap.from(".part2,.nav img",{
+    y:"-100%",
+    duration:1,
+    delay:.4,
+    opacity:0,
+    stagger:.5
+})
+
+gsap.from(".nav h5",{   
+    y:"-500%",
+    duration:1,
+    delay:.4
+    
+})
+gsap.from(".text",{   
+    x:"-400%",
+    duration:1,
+    delay:.5
+    
+})
+gsap.from(".small-img",{   
+    x:"200%",
+    duration:1,
+    delay:1.1
+    
+})
